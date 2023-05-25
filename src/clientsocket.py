e406c2d6b0e48f740525a6ae0462af15e1227899
tommy20gun <tommy20gun00@gmail.com>
2023-05-25 10:39:34 -0700

    working pi python code

--- src/clientsocket.py
+++ src/clientsocket.py
@@ -1,7 +1,8 @@
 import socket
+import time
 
 # Set the IP address and port of the Raspberry Pi server
-IP_ADDRESS = "192.168.147.134"
+IP_ADDRESS = "192.168.72.134"
 PORT = 1025
 
 # Connect to the server
@@ -9,9 +10,11 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP_ADDRESS, PORT))
 
 while True:
+    time.sleep(.5)
+    
     # Send data to the server
-    print("input data here: ")
-    data = input()
+    #print("input data here: ")
+    data = "M100\r\n"
     client_socket.sendall(data.encode())
 
     # Receive data from the server
--- src/savedonpi/serversocket.py
+++ src/savedonpi/serversocket.py
@@ -3,18 +3,18 @@ import serial
 import time
 
 # Set the IP address and port to listen on
-#IP_ADDRESS = "192.168.147.134"
+# IP_ADDRESS = "192.168.147.134"
 PORT = 1025
 
-#set up the serial port
+# Set up the serial port
 port = "/dev/ttyS0"
 baud_rate = 115200
 parity = 'N'
 stop_bits = 1
 
-ser= serial.Serial(port,baudrate = baud_rate,parity = parity,stopbits = stop_bits )
+ser = serial.Serial(port, baudrate=baud_rate, parity=parity, stopbits=stop_bits)
 
-data = "M15515"
+data = "M155\r\n"
 
 # Create a socket object and bind it to the IP address and port
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,18 +26,22 @@ server_socket.listen()
 # Accept a connection and receive data
 while True:
     client_socket, client_address = server_socket.accept()
-    received_data = client_socket.recv(1024).decode()
-    print("Received data: ", received_data)
+    
+    while True:
+        received_data = client_socket.recv(1024).decode()
+        
+        if not received_data:
+            # No more data received, break the inner loop
+            break
+        
+        print("Received data:", received_data)
 
-    # Send a response back to the client
-    response = f"Hello from the Raspberry Pi! msg: {received_data}"
-    client_socket.sendall(response.encode())
+        # Send a response back to the client
+        response = f"Hello from the Raspberry Pi! msg: {received_data}"
+        client_socket.sendall(response.encode())
 
-    #serial write data to the MCU
-    ser.write(data.encode())
+        # Serial write data to the MCU
+        ser.write(received_data.encode())
+        print("Sent data:", received_data)
 
-    # Close the client socket
     client_socket.close()
-
-
-
