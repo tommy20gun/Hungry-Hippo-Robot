690e3c0ced28f0171c91b64706682be182e9dd38
tommy20gun <tommy20gun00@gmail.com>
2023-06-08 14:12:51 -0700

    testing new main code

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from hsvcolordata import lower_ranges, upper_ranges, colors
 
 def main():
     #capture = cv.imread('Screenshot.png')
-    capture = cv.VideoCapture(1, cv.CAP_DSHOW)
+    capture = cv.VideoCapture(2, cv.CAP_DSHOW)
     cam_cleaner = CameraBufferCleanerThread(capture)
     rescalefactor = .7
 
@@ -26,10 +26,10 @@ def main():
     balls = None
 
     #initializes Socket object to send serial data
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     IP_ADDRESS = "192.168.84.134"
     PORT = 1025
-    s.connect((IP_ADDRESS, PORT))
+    s.connect((IP_ADDRESS, PORT))"""
 
     #initialize PID object
     pid = PID(.2,3,.01, setpoint= 0)
@@ -45,7 +45,6 @@ def main():
         #read the frames of the video as a while loop to make it "look" like a video
 
         #thereisaframe, frame = capture.read()
-
         #thereisaframe = True
         #if thereisaframe and not pause:
         if cam_cleaner.last_frame is not None:
@@ -59,9 +58,12 @@ def main():
             # Detect and draw balls of different colors
             for color in colors:
                 frame,ball_x, ball_y = detect_and_draw_balls(frame, lower_ranges[color], upper_ranges[color], colors[color])
-                if ball_x is not None:
-                    #adds balls to the list of balls
+                if ball_x:
                     balls.append(Ball(color,ball_x,ball_y))
+                    break
+                #if ball_x is not None:
+                    #adds balls to the list of balls
+                    #balls.append(Ball(color,ball_x,ball_y))
                     #print(balls)
                 #else:
                     #s.sendall("state3".encode())
@@ -94,9 +96,9 @@ def main():
             if data:
                 #this fixes the random noise of rotation
                 if not (data[8:10] == "D0" and previousdata[8:10] != "D0"):    
-                    s.sendall(data.encode())
+                    """s.sendall(data.encode())
                     received_data = s.recv(1024)
-                    print(received_data)
+                    print(received_data)"""
                     #print(data)
                 previousdata= data
 
@@ -108,9 +110,9 @@ def main():
         elif key == ord(' '):
             pause = not pause
             
-            s.sendall("pause".encode())
+            """s.sendall("pause".encode())
             received_data = s.recv(1024)
-            print(received_data)
+            print(received_data)"""
 
     capture.release()
     cv.destroyAllWindows()
@@ -119,3 +121,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+"""
+camera intiialization
+while true:
+    find 1 ball
+    cart pick up ball
+    if no more balls:
+        break
+    """
